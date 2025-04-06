d4bb278e75454f01648395a7ae7e98581ef2eeb3
DLuta <157763464+DLuta@users.noreply.github.com>
2025-04-06 19:57:12 +0300

    Update 100.py

--- 100.py
+++ 100.py
@@ -1,39 +1,56 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 import os
 import subprocess
-
-
 os.system('clear')
+def banner():
+    font = """	       ▄▄▄▄       ▄▄▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄▄▄  
+	     ▄█░░░░▌     ▐░░░░░░░░░▌  ▐░░░░░░░░░▌ 
+	    ▐░░▌▐░░▌    ▐░█░█▀▀▀▀▀█░▌▐░█░█▀▀▀▀▀█░▌
+	     ▀▀ ▐░░▌    ▐░▌▐░▌    ▐░▌▐░▌▐░▌    ▐░▌
+	        ▐░░▌    ▐░▌ ▐░▌   ▐░▌▐░▌ ▐░▌   ▐░▌
+	        ▐░░▌    ▐░▌  ▐░▌  ▐░▌▐░▌  ▐░▌  ▐░▌
+	        ▐░░▌    ▐░▌   ▐░▌ ▐░▌▐░▌   ▐░▌ ▐░▌
+	        ▐░░▌    ▐░▌    ▐░▌▐░▌▐░▌    ▐░▌▐░▌
+	    ▄▄▄▄█░░█▄▄▄ ▐░█▄▄▄▄▄█░█░▌▐░█▄▄▄▄▄█░█░▌
+	   ▐░░░░░░░░░░░▌ ▐░░░░░░░░░▌  ▐░░░░░░░░░▌ 
+	    ▀▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀    ▀▀▀▀▀▀▀▀▀  
+                                         Version: 0.1
+					 github:https://github.com/DLuta/100
+					 Coder :D.Luta
+  """
+    print(font)
 
-target = input("IP target: ")
+if __name__ == "__main__":
+    banner()
 
-if os.path.exists('scanresult'):
-    pass
-else:
-    os.mkdir('scanresult')
-
-os.system(f"rm -rf scanresult/{target}")
-with open(f"scanresult/{target}", 'a') as f:
-    f.write("###Target###\n")
-    f.write(f"###{target}###\n")
-    f.write("###GEOIP###\n")
-    geoip_output = subprocess.check_output(['geoiplookup', target]).decode('utf-8')
-    f.write(geoip_output)
-    ipinfo_output = subprocess.check_output(['curl', f'https://ipinfo.io/{target}']).decode('utf-8')
-    f.write(ipinfo_output)
+###CHECK DIRECTORY
+if not os.path.isdir("scanresult"): 
+	
+	# if the demo_folder2 directory is 
+	# not present then create it. 
+	os.makedirs("scanresult") 
 
 
-with open(f"scanresult/{target}", 'a') as f:
-    f.write("###DETECT OS###\n")
-    nmap_output = subprocess.check_output(['nmap', '-O', '--osscan-guess', '-Pn', target]).decode('utf-8')
-    f.write(nmap_output)
+target = input("		IP target:")
 
-nmap_output = subprocess.check_output(['nmap', '-Pn', '-sT', '--scan-delay', '1s', '-oG', f"scanresult/{target}.txt", '-p', '80,102,443,502,530,593,789,1089-1091,1911,1962,2222,2404,4000,4840,4843,4911,9600,19999,20000,20547,34962-34964,34980,44818,46823,46824,55000-55003', target]).decode('utf-8')
-with open(f"scanresult/{target}.txt", 'a') as f:
-    f.write(nmap_output)
+with open(f"scanresult/{target}", "w") as file:
+    file.write(f"		###Target###\n")
+    file.write(f"		###{target}###\n")
+    file.write(f"		###GEOIP###\n")
+    subprocess.run(["geoiplookup", target], stdout=file)
+    subprocess.run(["curl", f"https://ipinfo.io/{target}"], stdout=file)
 
-os.system(f"rm -rf scanresult/{target}.txt")
-os.system(f"more scanresult/{target}")
+# DETECT OS
+with open(f"scanresult/{target}", "a") as file:
+    file.write("		###DETECT OS###\n")
+    subprocess.run(["nmap", "-O", "--osscan-guess", "-Pn", target], stdout=file)
+
+subprocess.run(["nmap", "-Pn", "-sT", "--scan-delay", "1s", "-oG", f"scanresult/{target}.txt", "-p", "80,102,443,502,530,593,789,1089-1091,1911,1962,2222,2404,4000,4840,4843,4911,9600,19999,20000,20547,34962-34964,34980,44818,46823,46824,55000-55003", target])
+
+subprocess.run(["cat", f"scanresult/{target}.txt"], stdout=subprocess.PIPE)
+subprocess.run(["rm", "-rf", f"scanresult/{target}.txt"])
+subprocess.run(["more", f"scanresult/{target}"])
+
+
 
 
